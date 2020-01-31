0d2b7ce4f9d307d3c47cb3c7905e64bed7a70fde
Dawid Malenta <rezorl94@gmail.com>
2020-01-31 23:42:57 +0100

    initial commit

---
+++ src/test/java/com/rezorl/spring5webapp/Spring5webappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rezorl.spring5webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5webappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
